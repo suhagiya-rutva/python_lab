a32a8478181dbb6fe314ffdf40b58bca49576a39
suhagiya-rutva <suhagiyarutva2@gmail.com>
2026-01-31 08:06:28 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,16 @@
+# output using print()
+print("Hello World")
+
+
+#output using print()
+print("Hello Rutva")
+
+
+# input using input()
+name = input("Enter your name")
+print("Hello", name)
+
+
+# input using input()
+city = input("Enter your city")
+print("Hello", city)
