08d1d7ff3e517cbe0956fb53516cc451216e01bb
suhagiya-rutva <suhagiyarutva2@gmail.com>
2026-02-24 08:12:23 +0530

    Add files via upload

---
+++ Forloop.py
@@ -0,0 +1,26 @@
+for i in range (1,11):
+    print(i)
+
+for i in range (1,21):
+    if i%2 == 0:
+        print(i)
+
+for i in range(1,16):
+    if i%2!=0:
+        print(i)
+
+for i in range(1,11):
+    print("5 x",i,"=",5*i)
+
+name="Atmiya"
+for letter in name:
+    print(letter)
+
+total=0
+for i in range(1,6):
+    total=total+i
+    print("sum is:",total)
+
+numbers=[10,20,30,40]
+for n in numbers:
+    print(n)
